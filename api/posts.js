a0b3ecc518c615224aeda4264cd7bf9cd2f2fd03
Brianne Nunnelley <brianne.nyc@mac.com>
2022-04-06 21:13:34 -0500

    finished api/posts

--- api/posts.js
+++ api/posts.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const postsRouter = express.Router();
+const jwt = require('jsonwebtoken');
+const { requireUser } = require('./utils');
 
-const { getAllPosts } = require('../db');
+const { getAllPosts, createPost, getAllTags } = require('../db');
+const { user } = require('pg/lib/defaults');
 
 postsRouter.use((req, res, next) =>{
     console.log("A request is being made to /posts");
@@ -18,6 +21,42 @@ postsRouter.get('/', async (req, res) => {
         posts
     });
 });
+
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+    const { title, content, tags = "" } = req.body;
+    const auth = req.header('Authorization');
+  const tagArr = tags.trim().split(/\s+/)
+
+  const [, token] = auth.split(' ');
+
+  const recoveredData = jwt.verify(token, process.env.JWT_SECRET);
+ console.log(recoveredData)
+
+  const postData = {authorId: recoveredData.id, title: title, content: content, tags: []};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+      console.log(1)
+    // add authorId, title, content to postData object
+    const post = await createPost(postData);
+    console.log(2)
+    // this will create the post and the tags for us
+    // if the post comes back, res.send({ post });
+    res.send({ post })
+    // otherwise, next an appropriate error object 
+  } catch ({ error }) {
+    next({name: "oops", message: "post and tags broke"});
+  }
+});
+
+
+
+
 
 
 module.exports = postsRouter;
